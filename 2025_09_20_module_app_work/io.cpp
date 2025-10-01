b0e18d8fbfb0c83a601505100aa498643227aa5e
Bogomaz Alice <bogomaz.alice@gmail.com>
2025-10-01 17:07:46 +0300

    create module app

---
+++ 2025_09_20_module_app_work/io.cpp
@@ -0,0 +1,14 @@
+#include "io.hpp"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+void biv::print_array(const char* const comment, int* arr, const int size) {
+    
+    std::cout << comment;
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+}
