71ef466b7f99cfdec20d45ff313ab91a35b333dd
Alice <bogomaz.alice@gmail.com>
2025-10-19 17:38:00 +0300

    Update io.cpp

--- 2025_10_19_refactoring_homework/io.cpp
+++ 2025_10_19_refactoring_homework/io.cpp
@@ -1,15 +1,10 @@
 #include "io.hpp"
-
 #include <iostream>
-#include <vector>
 
-using namespace std;
-void biv::print_array(const char* const comment, const vector<int>& arr, const int size) {
-    
+void print::print_array(const char* const comment, const int* arr, int size) {
     std::cout << comment;
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";
     }
-    std::cout << std::endl;
-
+    std::cout << std::endl;	
 }
