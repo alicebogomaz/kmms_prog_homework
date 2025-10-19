f8754aa7a2ca39b65639c35d905941ae6c2ea4e5
Alice <bogomaz.alice@gmail.com>
2025-10-19 17:36:09 +0300

    Update main.cpp

--- 2025_10_19_refactoring_homework/main.cpp
+++ 2025_10_19_refactoring_homework/main.cpp
@@ -1,19 +1,13 @@
 #include "io.hpp"
 #include "sortings.hpp"
 
-#include <vector>
-
 int main() {
-
-    std::vector<int> arr = {1, 8, 2, 5, 3, 11};
+	int arr_size = 6;
+    int* arr = new int[]{1, 8, 2, 5, 3, 11};
 	
-	const int arr_size = sizeof(arr) / sizeof(arr[0]);
-	
-	biv::print_array("Массив до сортировки:", arr, arr_size);
-    
-    biv::sortings::mergeSort(arr, 0, arr.size() - 1);
-
-    biv::print_array("Массив после сортировки:", arr, arr_size);
+	print::print_array("Массив до сортировки:", arr, arr_size);
+    sort::mergeSort(arr, 0, arr_size - 1);
+    print::print_array("Массив после сортировки:", arr, arr_size);
 
     return 0;
 }
