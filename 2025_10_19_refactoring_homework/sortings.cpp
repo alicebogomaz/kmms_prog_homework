8d1b41ef0548876753711480dffdb14dded38f66
Alice <bogomaz.alice@gmail.com>
2025-10-19 17:37:15 +0300

    Update sortings.cpp

--- 2025_10_19_refactoring_homework/sortings.cpp
+++ 2025_10_19_refactoring_homework/sortings.cpp
@@ -6,11 +6,12 @@ void sort::merge(int* arr, int left, int mid, int right) {
 	int* L = new int[n1];
 	int* R = new int[n2];
 	
-	for (int i = 0; i < n1; ++i)
+	for (int i = 0; i < n1; ++i) {
 		L[i] = arr[left + i];
-	for (int j = 0; j < n2; ++ j)
+	}
+	for (int j = 0; j < n2; ++ j) {
 		R[j] = arr[mid + j + 1];
-	
+	}
 	int i = 0, j = 0, k = left;
 	
 	while (i < n1 && j < n2) {
