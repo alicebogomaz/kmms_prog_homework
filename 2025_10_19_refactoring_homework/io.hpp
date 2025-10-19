c70b254412249e0b452defe0c7c47c72a35f4c53
Alice <bogomaz.alice@gmail.com>
2025-10-19 17:38:19 +0300

    Update io.hpp

--- 2025_10_19_refactoring_homework/io.hpp
+++ 2025_10_19_refactoring_homework/io.hpp
@@ -1,10 +1,8 @@
 #ifndef IO_HPP
 #define IO_HPP
 
-#include <vector>
-
-namespace biv {
-	void print_array(const char* const comment, const std::vector<int>& arr, const int size);
+namespace print {
+	void print_array(const char* const comment, const int* arr, int size);
 }
 
 #endif
