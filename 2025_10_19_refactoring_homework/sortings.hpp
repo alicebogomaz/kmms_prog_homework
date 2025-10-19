44c6b2f1da8b36569c1f618375567ba7b36c177d
Alice <bogomaz.alice@gmail.com>
2025-10-19 17:37:36 +0300

    Update sortings.hpp

--- 2025_10_19_refactoring_homework/sortings.hpp
+++ 2025_10_19_refactoring_homework/sortings.hpp
@@ -1,13 +1,9 @@
 #ifndef SORTINGS_HPP
 #define SORTINGS_HPP
 
-#include <vector>
-
-namespace biv {
-	namespace sortings {
-		void merge(std::vector<int>& arr, int left, int mid, int right);
-		void mergeSort(std::vector<int>& arr, int left, int right);
-	}
+namespace sort {
+		void merge(int* arr, int left, int mid, int right);
+		void mergeSort(int* arr, int left, int right);
 }
 
 #endif
